20dac32a5e4637c249e4d6afd924e18aaf911a4b
hadriman <hadriman@live.fr>
2016-12-17 19:50:27 +0100

    methode predicteur/correcteur pour le calcul de fusion: quasiment le meme temps de calcul et aucun changement dans les résultats

--- Code/sim2.1.py
+++ Code/sim2.1.py
@@ -13,16 +13,17 @@ class terre:
     On considère que le chauffage provient uniquement de la radioactivité
     de l'Al26 
     """
-    def __init__(self, Ri, Ti, dt, size, cst=None):
+    def __init__(self, Ri, Rf, ta, beta, Ti, dt, size, cst=None):
         """
         R0 : rayon initial de la Terre en m
         T0 : temperature initiale en K
-        dt : pas de temps en fraction de tau_al
+        dt : pas de temps en My
         size : nombre de pas d'espace
         """
 
         if cst is None:
-            cst = { 'tau_al':  2.26E13,#s
+            cst = { #'tau_al':  0.717 ,#My
+                    'tau_al':  2.26E13, #s
                     'H0'    :  1.5E-7, #W kg-1
                     'T_neb' :  300   , #K
                     'sigma' :  5.67E-8,#W m-2 K-4
@@ -46,25 +47,37 @@ class terre:
         #ces constantes permettent de définir les 
         #paramètres d'adimensionnement
         self.t0 = cst['tau_al'] 
-        self.r0 = np.sqrt(cst['kT']*cst['tau_al']
-                /(cst['Cp']*cst['rho']))
+        self.r0 = Ri
         self.T0 = cst['T_neb'] 
         self.cst['Tafus_m'] = self.cst['Tfus_m']/self.T0
         self.cst['Tafus_s'] = self.cst['Tfus_s']/self.T0
 
         self.t = 0
-        self.dt = dt #!!Le param dt doit etre en fraction de tau_al!!
+        self.dt = dt/cst['tau_al'] 
+        self.ta = ta/cst['tau_al']
         self.c0 = self.dt*cst['tau_al']/(cst['rho']*cst['Cp']*cst['T_neb'])
 
-        print("rayon : {} km, dt : {} My, Ti : {} K, size {}".format(Ri*1E-3,dt*0.717,Ti,size))
+        print("rayon : {}->{} km (en {:.2}My), dt : {} My, Ti {} K, size : {},  ".format(Ri*1E-3,Rf*1E-3,self.ta*0.717,self.dt*0.717,Ti,size))
         
         #l'equation n'est pas definie en r=0 on decale donc le premier 
         # point de dr
-        dr = Ri/(self.r0*size)
-        r = np.linspace(dr, Ri/self.r0, num=size)
+        dr = 1/size
+        r = np.linspace(1/size, 1, num=size)
         self.r = r
         self.dr = dr
         self.size = size
+
+        self.R = Ri
+        self.Ri = Ri
+        self.beta = beta
+        if self.beta == 0 :
+            self.alpha = (Rf - Ri)/self.ta
+        elif self.beta == 1 : 
+            self.alpha = (np.log(Rf) - np.log(Ri))/self.ta
+        elif self.beta == 2 : 
+            self.alpha = (1/Ri - 1/Rf)/self.ta
+        elif self.beta == -1:
+            self.alpha = 0
         
         
         #condition initiales
@@ -80,45 +93,102 @@ class terre:
         eye = sparse.eye(size)
         d1 = sparse.dia_matrix(([1*ones,-1*ones],[0,1]),
                 shape=(size, size))
-        c1 = self.dt*((r+dr/2)/(r*dr))**2 
+        c1 = cst['tau_al']*cst['kT']*self.dt/(cst['rho']*cst['Cp'])*((r+dr/2)/(r*dr))**2 
+        #print("c1",c1)
+        #print(cst['tau_al'],cst['kT'],self.dt,(cst['rho']*cst['Cp']),((r+dr/2)/(r*dr))**2 )
         # on met c1 dans une diagonale pour la multiplication avec d1
         u1 = sparse.dia_matrix((c1,[0]), shape=(size, size))
         
         d2 = sparse.dia_matrix(([1*ones,-1*ones],[0,-1]),
                 shape=(size, size))
-        c2 = self.dt*((r-dr/2)/(r*dr))**2 
+        c2 = cst['tau_al']*cst['kT']*self.dt/(cst['rho']*cst['Cp'])*((r-dr/2)/(r*dr))**2 
         u2 = sparse.dia_matrix((c2,[0]), shape=(size, size))
 
+        d3 = sparse.dia_matrix(([1*ones,-1*ones],[-1,1]),
+               shape=(size, size))
+        c3 = self.dt*self.r/(2*dr) #cst['tau_al']*
+        u3 = sparse.dia_matrix((c3,[0]), shape=(size, size))
+
         #condition aux limites  
-        d1 = sparse.lil_matrix(d1)
+
+        d1 = sparse.lil_matrix(d1) 
         #d1 = d1.tocsr()
         d1[0,0:3]  = [2,-1,-1]
-        d1[-1:,-4:] = 0
-        d2 = sparse.lil_matrix(d2)
+        d1[-1:,-3:] = 0
+
+        
+        d2 = sparse.lil_matrix(d2) 
         #d2 = d2.tocsr()
         d2[0,0:2]  = 0
-        d2[-1,-3:] = [0,-2,2]
-        #print(d1.toarray())
-        #print(d2.toarray())
+        d2[-1,-3:] = [-1,-1,2]
+        
+        d3 = sparse.lil_matrix(d3) 
+        #d3 = d3.tocsr()
+        d3[0,0:2]  = [0,-0]
+        d3[-1,-2:] = [0,-0]
 
+
+        self.eye = eye
+        self.u1 = u1
+        self.d1 = d1
+        self.u2 = u2
+        self.d2 = d2
+        self.u3 = u3
+        self.d3 = d3
         self.c1 = c1
         self.c2 = c2
-        self.d1 = d1
-        self.d2 = d2
-
+        self.c3 = c3
+        
+        #print(d1.toarray())
+        #print(d2.toarray())
         #on construit m, la matrice complete 
-        self.m = eye + u1*d1 + u2*d2
+        self.m = (self.eye 
+                + (1/self.R**2)*self.u1*self.d1 
+                + (1/self.R**2)*self.u2*self.d2 
+                + (self.alpha*self.R**(self.beta-1))*self.u3*self.d3
+                )
+
+        # on ajoute la condition limite radiative
+        #self.m[-1,-2] = -1
+        #self.m[-2,-1] = 1
+        #self.m[-1,-1] = 1        
 
         #print(self.m.toarray())
         
         self.m = sparse.csc_matrix(self.m) #format csc plus efficace
         self.lu = linalg.splu(self.m) #on précalcule la décomposition LU
         
+    def update_m(self):
+        if self.beta == 0 :
+            self.R = self.Ri + self.alpha*self.t
+            #self.alpha = (Rf - Ri)/ta
+        elif self.beta == 1 : 
+            self.R = self.Ri * np.exp(self.alpha*self.t)
+            #self.alpha = (np.ln(Rf) - np.ln(Ri))/ta
+        elif self.beta == 2 : 
+            self.R = 1/(1/self.Ri - self.alpha*self.t)
+            #self.alpha = (1/Ri - 1/Rf)/ta
+        elif self.beta == -1:
+            self.R = float(self.Ri)
+        #print(self.R)
+        self.m = (self.eye 
+                + (1/self.R**2)*self.u1*self.d1 
+                + (1/self.R**2)*self.u2*self.d2 
+                + (self.alpha*self.R**(self.beta-1))*self.u3*self.d3)
+
     def update_P(self):
         cst = self.cst
         #On part de la chaleur radioactive
         self.P[:] = cst['rho']*cst['H0']*2**(-(self.t+self.dt))
-        self.P[-1] = 0
+        #!On oublie la radiation, on se contente d'injecter du materiau 
+        #à 300K a la surface!
+        #la convection à tendance à causer des grosses instabilités sinon
+        self.T[-2:] = 1 
+        self.P[-1:] = 0
+
+        return 
+
+        #Pour la sim2, ce qui suit n'est JAMAIS exécuté
         #On complete avec la radiation de corps noir à la surface
         try :
             self.cS
@@ -127,22 +197,29 @@ class terre:
             *self.dr*self.r0*cst['sigma']*(cst['T_neb']**4)
             /(self.cst['kT']*self.T0))
             #on utilise un point de temperature pour definir le flux radiatif
-        #print("T_rad",self.cS*(1-(self.T[-2]**4)))
-        self.T[-1] = self.T[-2] + self.cS*(1-(self.T[-2])**4)
-
-        #print("Cs",self.cS)
-
+        self.T[-1] = self.T[-2] + self.R*self.cS*(1-(self.T[-2])**4)
         # si la température du point fictif est plus petit que 0
         # la formule de la loi de Stephan n'est plus valable et la valeur 
         # calculée explose, il faut donc borner par zero pour que la 
         # diffusion puisse equilbrer 
         if self.T[-1] < 0 :
             self.T[-1] = 0
+        
         #print(self.T[-2:])
 
     def step(self):
         self.t += self.dt
+        self.lu = linalg.splu(self.m)
+        self.Ti = self.T.copy()
         self.T = self.lu.solve(self.T + self.c0*self.P)
+
+        self.fusion()
+        self.P_fusion = self.delta_phi_s*((1-cst['phi'])*cst['L_s'])/(self.T0*cst['Cp_s']) + self.delta_phi_m*(cst['phi']*cst['L_m'])/(self.T0*cst['Cp_m'])
+        self.T = self.lu.solve( self.Ti - self.P_fusion + self.c0*self.P)
+
+        self.fusion()
+        self.P_fusion += self.delta_phi_s*((1-cst['phi'])*cst['L_s'])/(self.T0*cst['Cp_s']) + self.delta_phi_m*(cst['phi']*cst['L_m'])/(self.T0*cst['Cp_m'])
+        self.T = self.lu.solve( self.Ti - self.P_fusion + self.c0*self.P)
 
     def fusion(self):
         """
@@ -154,6 +231,9 @@ class terre:
            l'excedant en chaleur
         """
         cst = self.cst 
+        phi_mi = self.phi_m.copy()
+        phi_si = self.phi_s.copy()
+
 
         fus_m = np.where((self.T>cst['Tafus_m'])*(self.phi_m < 1))
         sol_m = np.where((self.T<cst['Tafus_m'])*(self.phi_m > 0))
@@ -187,6 +267,11 @@ class terre:
 
         self.T[excess_sol] = self.T[excess_sol] + self.phi_s[excess_sol] * ((1-cst['phi'])*cst['L_s'])/(self.T0*cst['Cp_s'])
         self.phi_s[excess_sol] =  0
+
+        self.delta_phi_m = self.phi_m - phi_mi
+        self.delta_phi_s = self.phi_s - phi_si
+        #print(self.phi_m[:3],phi_mi[:3])
+        #print(self.phi_m[:3]-phi_mi[:3])
 
     def store(self):
         self.stored.append({
@@ -235,7 +320,6 @@ class terre:
             
             plt.pause(0.5)
 
-
 def test():
     """
     Test de la classe et comparaison des resultats avec une sol 
@@ -271,6 +355,28 @@ def test():
 	
 
     plt.legend()
+    plt.show()
+
+def test_R():
+    for beta in [0,1,2]:
+        R=[]
+        t = terre(Ri=5000,Rf=500000,ta=1,beta=beta,Ti=300,dt=0.01,size=1000)
+        for _ in range(100):
+            #print(t.R)
+            t.t += t.dt
+            t.update_m()
+            R.append(float(t.R))
+
+        R=np.array(R)
+        dRdt = R[1:]-R[:-1]
+        print(beta,t.alpha)
+        plt.figure()
+        plt.title("beta  = {}".format(beta))
+        plt.plot(R/R[-1],label="$R$")
+        plt.plot(dRdt/dRdt[-1],label="$dR/dt$")
+        plt.plot((R[1:]/R[-1])**beta,'r--',label=r"$R^{\beta}$")
+        plt.legend(loc="best")
+
     plt.show()
 
 def test_fusion():
@@ -310,14 +416,13 @@ if __name__ == '__main__' :
 
     #test_fusion()
 
-    cst = { 'tau_al':  2.2611E13, #s
+    #test_R()
+
+    cst = { 'tau_al':  2.26E13,
             'H0'    :  1.5E-7, #W kg-1
             'T_neb' :  300   , #K
             'sigma' :  5.67E-8,#W m-2 K-4
-            #'sigma' :  0,#W m-2 K-4
-            #'sigma' :  0,#W m-2 K-4
             'phi'   :  0.18  , # sans unité
-            #'kT'    :  11.48 , #W K-1 m-1
             'kT'    :  11.48 , #W K-1 m-1
             'kT_m'  :  50    , #W K-1 m-1
             'kT_s'  :  3     , #W K-1 m-1
@@ -334,22 +439,18 @@ if __name__ == '__main__' :
             #'L_m'   :  1 , #J kg-1
             #'L_s'   :  1 , #J kg-1
             }
-    t = terre(Ri=1000,Ti=300,dt=0.0001,size=1000,cst=cst)
-    #t.t = 1/0.717
+    t = terre(Ri=50000,Rf=500000,ta=1*cst['tau_al'],beta=1,Ti=200,dt=5E-4*cst['tau_al'],size=1000,cst=cst)
+    print(t.alpha,t.beta)
     plt.figure(figsize=(6,8))
-    for _ in range(100):
-        for __ in range(1000):
+    n = int(t.ta/(5*t.dt))
+    for _ in range(5):
+        for __ in range(n):
             t.update_P()
+            t.update_m()
             #t.P[:]=0
             t.step()
-            t.fusion()
-            print("t: {} My".format(t.t*0.717))
-            """
-            if t.t*0.74 > 1.0 :
-                break
-        if t.t*0.74 > 1.0 :
-            break
-            """
+            #t.fusion()
+            print("R: {:.3}  , t: {:.3} My".format(t.R,t.t*0.717))
 
         plt.plot(t.T[:-1]*t.T0,t.r[:-1]/t.r[-2])
         #plt.plot(t.phi_m,'-')
--- Code/sim3.1.py
+++ Code/sim3.1.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import sparse
 from scipy.sparse  import linalg
+from scipy.interpolate import interp1d
 
 
 
@@ -13,16 +14,19 @@ class terre:
     On considère que le chauffage provient uniquement de la radioactivité
     de l'Al26 
     """
-    def __init__(self, Ri, Ti, dt, size, cst=None):
+    def __init__(self, Ri, Rf, ta, beta, Ti, dt, size, T_imp=None, cst=None):
         """
         R0 : rayon initial de la Terre en m
         T0 : temperature initiale en K
-        dt : pas de temps en fraction de tau_al
+        dt : pas de temps en My
         size : nombre de pas d'espace
         """
 
         if cst is None:
-            cst = { 'tau_al':  2.26E13,#s
+            cst = { #'tau_al':  0.717 ,#My
+                    'f'     :  0.2   , #20%
+                    'G'     : 6.67E-11,#N m2 kg-2
+                    'tau_al':  2.26E13, #s
                     'H0'    :  1.5E-7, #W kg-1
                     'T_neb' :  300   , #K
                     'sigma' :  5.67E-8,#W m-2 K-4
@@ -46,26 +50,59 @@ class terre:
         #ces constantes permettent de définir les 
         #paramètres d'adimensionnement
         self.t0 = cst['tau_al'] 
-        self.r0 = np.sqrt(cst['kT']*cst['tau_al']
-                /(cst['Cp']*cst['rho']))
+        self.r0 = Ri
         self.T0 = cst['T_neb'] 
         self.cst['Tafus_m'] = self.cst['Tfus_m']/self.T0
         self.cst['Tafus_s'] = self.cst['Tfus_s']/self.T0
 
         self.t = 0
-        self.dt = dt #!!Le param dt doit etre en fraction de tau_al!!
+        self.dt = dt/cst['tau_al'] 
+        self.ta = ta/cst['tau_al']
         self.c0 = self.dt*cst['tau_al']/(cst['rho']*cst['Cp']*cst['T_neb'])
 
-        print("rayon : {} km, dt : {} My, Ti : {} K, size {}".format(Ri*1E-3,dt*0.717,Ti,size))
+        print("rayon : {}->{} km (en {:.2}My), dt : {} My, Ti {} K, size : {},  ".format(Ri*1E-3,Rf*1E-3,self.ta*0.717,self.dt*0.717,Ti,size))
         
         #l'equation n'est pas definie en r=0 on decale donc le premier 
         # point de dr
-        dr = Ri/(self.r0*size)
-        r = np.linspace(dr, Ri/self.r0, num=size)
+        dr = 1/size
+        r = np.linspace(1/size, 1, num=size)
         self.r = r
         self.dr = dr
         self.size = size
+
+        self.R = Ri
+        self.Ri = Ri
+        self.beta = beta
+        if self.beta == 0 :
+            self.alpha = (Rf - Ri)/self.ta
+        elif self.beta == 1 : 
+            self.alpha = (np.log(Rf) - np.log(Ri))/self.ta
+        elif self.beta == 2 : 
+            self.alpha = (1/Ri - 1/Rf)/self.ta
+        elif self.beta == -1:
+            self.alpha = 0
         
+        
+        if isinstance(T_imp, str):
+            print("T_imp from {}".format(T_imp))
+            T_imp = np.load(T_imp)
+            T_imp[0] = T_imp[0]/0.717 #My -> adim
+            self.T_imp = interp1d(T_imp[0],T_imp[4+beta],kind='linear',
+                    fill_value="extrapolate",assume_sorted=True) 
+
+        elif isinstance(T_imp, int) or isinstance(T_imp, float) : 
+            print("T_imp = {}K".format(T_imp))
+            T_imp = np.ones((7,2))*T_imp
+            T_imp[0] = [0,100]
+            self.T_imp = interp1d(T_imp[0],T_imp[4+beta],kind='nearest',
+                    fill_value="extrapolate",assume_sorted=True) 
+
+        elif T_imp is None : 
+            print("T_imp = 1000K")
+            T_imp = np.ones((7,2))*1000
+            T_imp[0] = [0,100]
+            self.T_imp = interp1d(T_imp[0],T_imp[4+beta],kind='nearest',
+                    fill_value="extrapolate",assume_sorted=True) 
         
         #condition initiales
         self.T = (Ti/self.T0)*np.ones(size)
@@ -80,45 +117,130 @@ class terre:
         eye = sparse.eye(size)
         d1 = sparse.dia_matrix(([1*ones,-1*ones],[0,1]),
                 shape=(size, size))
-        c1 = self.dt*((r+dr/2)/(r*dr))**2 
+        c1 = cst['tau_al']*cst['kT']*self.dt/(cst['rho']*cst['Cp'])*((r+dr/2)/(r*dr))**2 
+        #print("c1",c1)
+        #print(cst['tau_al'],cst['kT'],self.dt,(cst['rho']*cst['Cp']),((r+dr/2)/(r*dr))**2 )
         # on met c1 dans une diagonale pour la multiplication avec d1
         u1 = sparse.dia_matrix((c1,[0]), shape=(size, size))
         
         d2 = sparse.dia_matrix(([1*ones,-1*ones],[0,-1]),
                 shape=(size, size))
-        c2 = self.dt*((r-dr/2)/(r*dr))**2 
+        c2 = cst['tau_al']*cst['kT']*self.dt/(cst['rho']*cst['Cp'])*((r-dr/2)/(r*dr))**2 
         u2 = sparse.dia_matrix((c2,[0]), shape=(size, size))
 
+        d3 = sparse.dia_matrix(([1*ones,-1*ones],[-1,1]),
+               shape=(size, size))
+        c3 = self.dt*self.r/(2*dr) #cst['tau_al']*
+        u3 = sparse.dia_matrix((c3,[0]), shape=(size, size))
+
         #condition aux limites  
-        d1 = sparse.lil_matrix(d1)
+
+        d1 = sparse.lil_matrix(d1) 
         #d1 = d1.tocsr()
         d1[0,0:3]  = [2,-1,-1]
-        d1[-1:,-4:] = 0
-        d2 = sparse.lil_matrix(d2)
+        d1[-1:,-3:] = 0
+
+        
+        d2 = sparse.lil_matrix(d2) 
         #d2 = d2.tocsr()
         d2[0,0:2]  = 0
-        d2[-1,-3:] = [0,-2,2]
-        #print(d1.toarray())
-        #print(d2.toarray())
+        d2[-1,-3:] = [-1,-1,2]
+        
+        d3 = sparse.lil_matrix(d3) 
+        #d3 = d3.tocsr()
+        d3[0,0:2]  = [0,-0]
+        d3[-1,-2:] = [0,-0]
 
+
+        self.eye = eye
+        self.u1 = u1
+        self.d1 = d1
+        self.u2 = u2
+        self.d2 = d2
+        self.u3 = u3
+        self.d3 = d3
         self.c1 = c1
         self.c2 = c2
-        self.d1 = d1
-        self.d2 = d2
-
+        self.c3 = c3
+        
+        #print(d1.toarray())
+        #print(d2.toarray())
         #on construit m, la matrice complete 
-        self.m = eye + u1*d1 + u2*d2
+        self.m = (self.eye 
+                + (1/self.R**2)*self.u1*self.d1 
+                + (1/self.R**2)*self.u2*self.d2 
+                + (self.alpha*self.R**(self.beta-1))*self.u3*self.d3
+                )
+
+        # on ajoute la condition limite radiative
+        #self.m[-1,-2] = -1
+        #self.m[-2,-1] = 1
+        #self.m[-1,-1] = 1        
 
         #print(self.m.toarray())
         
         self.m = sparse.csc_matrix(self.m) #format csc plus efficace
         self.lu = linalg.splu(self.m) #on précalcule la décomposition LU
         
+    def update_m(self):
+        if self.beta == 0 :
+            self.R = self.Ri + self.alpha*self.t
+            #self.alpha = (Rf - Ri)/ta
+        elif self.beta == 1 : 
+            self.R = self.Ri * np.exp(self.alpha*self.t)
+            #self.alpha = (np.ln(Rf) - np.ln(Ri))/ta
+        elif self.beta == 2 : 
+            self.R = 1/(1/self.Ri - self.alpha*self.t)
+            #self.alpha = (1/Ri - 1/Rf)/ta
+        elif self.beta == -1:
+            self.R = float(self.Ri)
+        #print(self.R)
+        self.m = (self.eye 
+                + (1/self.R**2)*self.u1*self.d1 
+                + (1/self.R**2)*self.u2*self.d2 
+                + (self.alpha*self.R**(self.beta-1))*self.u3*self.d3)
+
     def update_P(self):
         cst = self.cst
+        DR=self.R/5
+
         #On part de la chaleur radioactive
-        self.P[:] = cst['rho']*cst['H0']*2**(-(self.t+self.dt))
-        self.P[-1] = 0
+        self.P[:] = cst['rho']*cst['H0']*2**(-(self.t))
+        """
+        print(int(self.size*DR/self.R))
+        print("a",cst['f']*cst['rho']*self.alpha*(self.R**(self.beta-1))/DR)
+        print("b",(4/3)*np.pi*cst['G']*cst['rho']*self.alpha*(self.R**(self.beta+2)))
+        print("c",cst['Cp']*(self.T_imp(self.t)-cst['T_neb']))
+        print("d",self.alpha*(self.R**self.beta))
+        print("e",self.T_imp(self.t))
+
+        print("i",self.P[-1])
+        print(self.T_imp(self.t))
+        print(cst['T_neb'])
+
+        """
+        """
+        h0 = cst['f']*cst['rho']*cst['Cp']*(
+                self.T_imp(self.t) - cst['T_neb'])
+        #h1 = 
+        """
+        self.P[-int(self.size*DR/self.R):] += (
+            cst['f']*cst['rho']*self.alpha*self.R**self.beta
+            /(DR*cst['tau_al'])
+            *( (4/3)*np.pi*cst['G']*cst['rho']*self.R**2 
+               + cst['Cp']*(self.T_imp(self.t)-cst['T_neb'])
+             )
+            )
+        #print("f",self.P[-1])
+        #!On oublie la radiation, on se contente d'injecter du materiau 
+        #à 300K a la surface!
+        #la convection à tendance à causer des grosses instabilités sinon
+        self.T[-2:] = 1 
+        self.P[-1:] = 0
+
+        return 
+
+        #Pour la sim2, ce qui suit n'est JAMAIS exécuté
         #On complete avec la radiation de corps noir à la surface
         try :
             self.cS
@@ -127,24 +249,88 @@ class terre:
             *self.dr*self.r0*cst['sigma']*(cst['T_neb']**4)
             /(self.cst['kT']*self.T0))
             #on utilise un point de temperature pour definir le flux radiatif
-        #print("T_rad",self.cS*(1-(self.T[-2]**4)))
-        self.T[-1] = self.T[-2] + self.cS*(1-(self.T[-2])**4)
-
-        #print("Cs",self.cS)
-
+        self.T[-1] = self.T[-2] + self.R*self.cS*(1-(self.T[-2])**4)
         # si la température du point fictif est plus petit que 0
         # la formule de la loi de Stephan n'est plus valable et la valeur 
         # calculée explose, il faut donc borner par zero pour que la 
         # diffusion puisse equilbrer 
         if self.T[-1] < 0 :
             self.T[-1] = 0
+        
         #print(self.T[-2:])
+
 
     def step(self):
         self.t += self.dt
+        self.lu = linalg.splu(self.m)
+        self.Ti = self.T.copy()
         self.T = self.lu.solve(self.T + self.c0*self.P)
 
+        self.fusion()
+        self.P_fusion = self.delta_phi_s*((1-cst['phi'])*cst['L_s'])/(self.T0*cst['Cp_s']) + self.delta_phi_m*(cst['phi']*cst['L_m'])/(self.T0*cst['Cp_m'])
+        self.T = self.lu.solve( self.Ti - self.P_fusion + self.c0*self.P)
+
+        self.fusion()
+        self.P_fusion += self.delta_phi_s*((1-cst['phi'])*cst['L_s'])/(self.T0*cst['Cp_s']) + self.delta_phi_m*(cst['phi']*cst['L_m'])/(self.T0*cst['Cp_m'])
+        self.T = self.lu.solve( self.Ti - self.P_fusion + self.c0*self.P)
+
     def fusion(self):
+        """
+        Cette fonction calcule la modification du ratio solide/liquide
+        en deux etapes : 
+        1. Si la condition pour un changement d'état est verifiée
+           on utilise toute la chaleur pour changer d'état
+        2. Si on a plus de 100% de liquide ou de solide on rebalance
+           l'excedant en chaleur
+        """
+        cst = self.cst 
+        phi_mi = self.phi_m.copy()
+        phi_si = self.phi_s.copy()
+
+
+        fus_m = np.where((self.T>cst['Tafus_m'])*(self.phi_m < 1))
+        sol_m = np.where((self.T<cst['Tafus_m'])*(self.phi_m > 0))
+        tot_m = [np.concatenate([fus,sol]) for fus,sol in zip(fus_m,sol_m)]
+
+        self.phi_m[tot_m] = self.phi_m[tot_m] + (self.T[tot_m] - cst['Tafus_m']) * self.T0*cst['Cp_m']/(cst['phi']*cst['L_m'])
+        self.T[tot_m] = cst['Tafus_m']
+
+
+        excess_fus = np.where(self.phi_m > 1)
+        excess_sol = np.where(self.phi_m < 0)
+
+        self.T[excess_fus] = self.T[excess_fus] + (self.phi_m[excess_fus] - 1) * (cst['phi']*cst['L_m'])/(self.T0*cst['Cp_m'])
+        self.phi_m[excess_fus] =  1
+
+        self.T[excess_sol] = self.T[excess_sol] + self.phi_m[excess_sol] * (cst['phi']*cst['L_m'])/(self.T0*cst['Cp_m'])
+        self.phi_m[excess_sol] =  0
+
+        fus_s = np.where((self.T>cst['Tafus_s'])*(self.phi_s < 1))
+        sol_s = np.where((self.T<cst['Tafus_s'])*(self.phi_s > 0))
+        tot_s = [np.concatenate([fus,sol]) for fus,sol in zip(fus_s,sol_s)]
+
+        self.phi_s[tot_s] = self.phi_s[tot_s] + (self.T[tot_s] - cst['Tafus_s']) * self.T0*cst['Cp_s']/((1-cst['phi'])*cst['L_s'])
+        self.T[tot_s] = cst['Tafus_s']
+
+        excess_fus = np.where(self.phi_s > 1)
+        excess_sol = np.where(self.phi_s < 0)
+
+        self.T[excess_fus] = self.T[excess_fus] + (self.phi_s[excess_fus] - 1) * ((1-cst['phi'])*cst['L_s'])/(self.T0*cst['Cp_s'])
+        self.phi_s[excess_fus] =  1
+
+        self.T[excess_sol] = self.T[excess_sol] + self.phi_s[excess_sol] * ((1-cst['phi'])*cst['L_s'])/(self.T0*cst['Cp_s'])
+        self.phi_s[excess_sol] =  0
+
+        self.delta_phi_m = self.phi_m - phi_mi
+        self.delta_phi_s = self.phi_s - phi_si
+        #print(self.phi_m[:3],phi_mi[:3])
+        #print(self.phi_m[:3]-phi_mi[:3])
+
+    def stepi_bak(self):
+        self.t += self .dt
+        self.T = linalg.spsolve(self.m, self.T + self.c0*self.P)
+
+    def fusion_bak(self):
         """
         Cette fonction calcule la modification du ratio solide/liquide
         en deux etapes : 
@@ -236,6 +422,7 @@ class terre:
             plt.pause(0.5)
 
 
+
 def test():
     """
     Test de la classe et comparaison des resultats avec une sol 
@@ -271,6 +458,28 @@ def test():
 	
 
     plt.legend()
+    plt.show()
+
+def test_R():
+    for beta in [0,1,2]:
+        R=[]
+        t = terre(Ri=5000,Rf=500000,ta=1,beta=beta,Ti=300,dt=0.01,size=1000)
+        for _ in range(100):
+            #print(t.R)
+            t.t += t.dt
+            t.update_m()
+            R.append(float(t.R))
+
+        R=np.array(R)
+        dRdt = R[1:]-R[:-1]
+        print(beta,t.alpha)
+        plt.figure()
+        plt.title("beta  = {}".format(beta))
+        plt.plot(R/R[-1],label="$R$")
+        plt.plot(dRdt/dRdt[-1],label="$dR/dt$")
+        plt.plot((R[1:]/R[-1])**beta,'r--',label=r"$R^{\beta}$")
+        plt.legend(loc="best")
+
     plt.show()
 
 def test_fusion():
@@ -310,14 +519,15 @@ if __name__ == '__main__' :
 
     #test_fusion()
 
-    cst = { 'tau_al':  2.2611E13, #s
+    #test_R()
+
+    cst = { 'f'     :    0.2 ,#20%
+            'G'     : 6.67E-11,#N m2 kg-2
+            'tau_al':  2.26E13,
             'H0'    :  1.5E-7, #W kg-1
             'T_neb' :  300   , #K
             'sigma' :  5.67E-8,#W m-2 K-4
-            #'sigma' :  0,#W m-2 K-4
-            #'sigma' :  0,#W m-2 K-4
             'phi'   :  0.18  , # sans unité
-            #'kT'    :  11.48 , #W K-1 m-1
             'kT'    :  11.48 , #W K-1 m-1
             'kT_m'  :  50    , #W K-1 m-1
             'kT_s'  :  3     , #W K-1 m-1
@@ -334,22 +544,19 @@ if __name__ == '__main__' :
             #'L_m'   :  1 , #J kg-1
             #'L_s'   :  1 , #J kg-1
             }
-    t = terre(Ri=1000,Ti=300,dt=0.0001,size=1000,cst=cst)
-    #t.t = 1/0.717
+    t = terre(Ri=50000,Rf=500000,ta=1*cst['tau_al'],beta=1,Ti=200,dt=5E-4*cst['tau_al'],size=1000,cst=cst)
+    print(t.alpha,t.beta)
     plt.figure(figsize=(6,8))
-    for _ in range(100):
-        for __ in range(1000):
+    n = int(t.ta/(5*t.dt))
+    for _ in range(5):
+        for __ in range(n):
             t.update_P()
+            t.update_m()
             #t.P[:]=0
             t.step()
-            t.fusion()
-            print("t: {} My".format(t.t*0.717))
-            """
-            if t.t*0.74 > 1.0 :
-                break
-        if t.t*0.74 > 1.0 :
-            break
-            """
+            #t.fusion()
+            print("R: {:.6}  , t: {:.3} My".format(t.R,t.t*0.717))
+            #raise Exception
 
         plt.plot(t.T[:-1]*t.T0,t.r[:-1]/t.r[-2])
         #plt.plot(t.phi_m,'-')
